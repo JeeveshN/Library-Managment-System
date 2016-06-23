fa0df9cfd9a60ccc63e978846a34b641571660f1
JeeveshN <narang.jeevesh@gmail.com>
2016-06-24 02:26:41 +0530

    Add Books and View All

--- library.py
+++ library.py
@@ -19,7 +19,7 @@ class Book(db1.Document):
     author=db1.StringField()
     quantity=db1.IntField()
     section=db1.StringField()
-    issued_by=db1.ListField(db1.DictField(db1.StringField()))
+    serialno=db1.IntField()
 
 class Information(db1.Document):
     name=db1.StringField()
@@ -33,6 +33,13 @@ class Admin(db1.Document):
 if not Admin.query.filter(Admin.username=='admin').first():
     Admin=Admin(username='admin',password='admin')
     Admin.save()
+
+def check_admin():
+    if 'username' in session:
+        username=session['username']
+        if username==Admin.query.one().username:
+            return True
+    return False
 
 @app.route('/')
 def login():
@@ -83,6 +90,8 @@ def admin123():
 
 @app.route('/admin',methods=['POST','GET'])
 def admin():
+    if check_admin():
+        render_template("admin-login.html")
     if request.method == 'POST':
         if not request.form['username'] or not request.form['password']:
             flash("Please fill in All The Fields")
@@ -102,5 +111,40 @@ def admin_logout():
     session.pop('username',None)
     return redirect(url_for('admin123'))
 
+@app.route('/all_books')
+def all_books():
+    if check_admin():
+        return render_template('all.html',books=Book.query.all())
+    else:
+        return redirect(url_for('admin123'))
+
+@app.route('/add')
+def add():
+    if check_admin():
+        return render_template('add-books.html')
+    else:
+        return redirect(url_for('admin123'))
+
+@app.route('/add_books',methods=['POST','GET'])
+def add_books():
+    if check_admin():
+        if request.method == 'POST':
+            if not request.form['name'] or not request.form['author'] or not request.form['quantity'] or not request.form['section'] or not request.form['serialno']:
+                flash("Please Fill All Fields")
+                return render_template('add-books.html')
+            elif Book.query.filter(Book.name==request.form['name']).first() and Book.query.filter(Book.author==request.form['author']).first():
+                book=Book.query.filter(Book.name==request.form['name']).first()
+                book.quantity=book.quantity+int(request.form['quantity'])
+                flash("The Book has been Successfully Added")
+            else:
+                book=Book(name=request.form['name'],author=request.form['author'],quantity=int(request.form['quantity']),section=request.form['section'],serialno=int(request.form['serialno']))
+                book.save()
+                flash("The Book has been Successfully Added")
+    return redirect(url_for('admin123'))
+#@app.route('/delete_all',methods=['POST','GET'])
+##def delete_all():
+##    for book in Book.query.all():
+##        book.remove()
+##    return redirect(url_for('all_books'))
 if __name__ == '__main__':
     app.run(debug=True)
